76aab1cdfc258594e68aaba0661f565ac4bb47cb
Eliseo Martelli <me@eliseomartelli.it>
2024-07-09 15:51:48 +0200

    feat: add new environment variable

--- env.ts
+++ env.ts
@@ -4,3 +4,5 @@ export const TOOL_DIR = resolve(
   Deno.env.get("TOOL_DIR") ||
     "/srv/galaxy/server/tools/custom",
 );
+
+export const GIT_REVISION = Deno.env.get("DENO_DEPLOYMENT_ID") || "unknown";
