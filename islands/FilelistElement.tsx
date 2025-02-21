0f33b43d0af7937f0bc302717db363e03987ec26
Eliseo Martelli <me@eliseomartelli.it>
2025-02-21 16:08:45 +0100

    fix: change window to globalThis

--- islands/FilelistElement.tsx
+++ islands/FilelistElement.tsx
@@ -4,10 +4,11 @@ export function FilelistElement({ name }: Tool) {
   return (
     <>
       <button
+        type="button"
         className="font-bold bg-red-500 text-white py-1 px-2 rounded-md"
         onClick={async () => {
           await fetch(`/api/delete/${name}`, { method: "DELETE" });
-          window.location.reload();
+          globalThis.location.reload();
         }}
       >
         Delete
